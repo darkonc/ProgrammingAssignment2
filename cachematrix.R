c925733faadf0d6fd2d50917178e08b9f165f6b4
darkonc <samuel@bcgreen.com>
2015-12-26 18:08:53 -0500

    Cleaned up the documentation a bit.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## MakeCachematrix() takes  a matrix and returns a list of
+#### MakeCachematrix() takes  a matrix and returns a list of
 ## functions to work with the matrix.
-## set(aMatrix) assigns a new value to the stored matrix (and clears
+##
+### set(aMatrix) assigns a new value to the stored matrix (and clears
 ## the cached inverse)
 ##
-## get() simply returns the value of the cached matrix
+### get() simply returns the value of the cached matrix
 ## 
-## getInverse returns the (possibly cached) value for the inverse matrix.
+### getInverse() returns the (possibly cached) value for the inverse matrix.
 ## 
 ## Note that I don't have a setInverse() function, since that
 ## work is done with getInverse() .. and I don't have to 
@@ -31,7 +32,7 @@ makeCacheMatrix <- function(theMatrix = matrix()) {
 	    # }else{
 	    #  print("using cache")
 	    }
-	  theInverse # return inverse 
+	  theInverse # return inverse   
 	}
 	
 	list( get=get,set=set,getInverse=getInverse)
@@ -47,6 +48,6 @@ makeCacheMatrix <- function(theMatrix = matrix()) {
 ## pass on to solve()
 
 cacheSolve <- function(matObj, ...) {
-        ## Return a matrix that is the inverse of 'matObj'
+        ## Return a (cached?) matrix that is the inverse of 'matObj'
 	matObj$getInverse(...)
 }
