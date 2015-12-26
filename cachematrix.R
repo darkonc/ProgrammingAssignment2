ed45f1799c7016161f2cfd175c6a461cf81a97f2
darkonc <samuel@bcgreen.com>
2015-12-26 16:46:21 -0500

    'standard' version of cachematrix (with the work being done in cachesolve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## MakeCachematrix() takes  a matrix and returns a list of
+## functions to work with the matrix.
+## set(aMatrix) assigns a new value to the stored matrix (and clears
+## the cached inverse)
+##
+## get() simply returns the value of the cached matrix
+##
+## setInvese(aMatrix) sets a cached value for the inverse matrix
+## 
+## getInverse returns the cached value for the inverse matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(theMatrix = matrix()) {
+	theInverse <- NULL
+	get= function(){
+	    theMatrix
+	}
+	set= function(newMatrix){
+	    theMatrix<<- newMatrix
+	    theInverse<<- NULL
+	}
+	getInverse= function() {
+	    theInverse
+	}
+	setInverse= function(newInverse){
+	    theInverse <<- newInverse
+	}
+	list( get=get,set=set,getInverse=getInverse,setInverse=setInverse)
 }
 
 
-## Write a short comment describing this function
+## Cachesolve takes a cached-Matrix object (matObj)
+## from MakeCacheMatrix and returns the inverse of the matrix
+## if the invese has already been calculated and returned, then 
+## Cachesolve returns the cached value, otherwise it calculates
+## a new inverse, caches that, and returns the (newly cached) inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matObj, ...) {
+        ## Return a matrix that is the inverse of 'matObj'
+	theResult=matObj$getInverse()
+	if(is.null(theResult)){
+	    print("setting cache")
+	    theResult= solve(matObj$get(),...)
+	    matObj$setInverse(theResult)
+	}else{
+	    print("using cache")
+	}
+	    theResult
 }
